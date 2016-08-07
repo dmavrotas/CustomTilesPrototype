8a4c2d215afd1b9545a511722b7b3460bf33dfcb
DMavrotas <dmavrotas@hotmail.com>
2016-08-07 23:51:21 +0200

    Still huge way to go

---
+++ CustomTilesPrototype/Controls/DashboardControl.xaml.cs
@@ -0,0 +1,73 @@
+﻿using CustomTilesPrototype.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomTilesPrototype.Controls
+{
+    /// <summary>
+    /// Interaction logic for DashboardControl.xaml
+    /// </summary>
+    public partial class DashboardControl : UserControl, INotifyPropertyChanged
+    {
+        #region Members
+
+        private DashboardViewModel _viewModel;
+
+        public DashboardViewModel ViewModel
+        {
+            get { return _viewModel; }
+            set
+            {
+                _viewModel = value;
+                NotifyPropertyChanged("ViewModel");
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(info));
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public DashboardControl()
+        {
+            InitializeComponent();
+            InitializeProperties();
+            DataContext = ViewModel;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void InitializeProperties()
+        {
+            ViewModel = new DashboardViewModel();
+            //TileClicked = new RelayCommand<short>(PerformClick);
+        }
+
+        #endregion
+    }
+}
---
+++ CustomTilesPrototype/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,78 @@
+﻿using CustomTilesPrototype.Controls;
+using GalaSoft.MvvmLight;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace CustomTilesPrototype.ViewModels
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class DashboardViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+        #region Members
+
+        private ObservableCollection<GenericTile> _tiles;
+
+        public ObservableCollection<GenericTile> Tiles
+        {
+            get { return _tiles; }
+            set
+            {
+                _tiles = value;
+                NotifyPropertyChanged("Tiles");
+            }
+        }
+
+        private Orientation _dashboardOrientation;
+
+        public Orientation DashboardOrientation
+        {
+            get { return _dashboardOrientation; }
+            set
+            {
+                _dashboardOrientation = value;
+                NotifyPropertyChanged("DashboardOrientation");
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(info));
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the DashboardViewModel class.
+        /// </summary>
+        public DashboardViewModel()
+        {
+            Tiles = new ObservableCollection<GenericTile>();
+            Tiles.Add(new GenericTile());
+            Tiles.Add(new GenericTile());
+            DashboardOrientation = Orientation.Horizontal;
+        }
+
+        #endregion
+
+        #region Events
+
+
+
+        #endregion
+    }
+}
